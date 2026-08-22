ca2c3d3ad62d3e01c9d3db0353760218593c649c
harish-backend-dev <harish.backend.dev@gmail.com>
2026-08-22 12:17:50 +0530

    Update QualityGovernanceStrip.tsx

--- src/components/common/QualityGovernanceStrip.tsx
+++ src/components/common/QualityGovernanceStrip.tsx
@@ -28,10 +28,12 @@ export function QualityGovernanceStrip({
           </div>
         </div>
         <div className="dom-care-cqc__rating quality-governance-strip__rating">
+          <a href="https://www.cqc.org.uk/location/1-1285309099" target="_blank">
           <AssetImage name="dom-care-cqc-rating-logo.svg" alt="Care Quality Commission" />
           <i aria-hidden="true" />
           <span>Overall: <strong>Good</strong></span>
           <AssetImage className="dom-care-cqc__external" name="dom-care-cqc-external.svg" alt="" aria-hidden="true" />
+            </a>
         </div>
       </div>
     </section>
